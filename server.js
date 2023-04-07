beeb82f2a4e6bff5aa2e7c64c32f4af4ac4ecb5b
phoenix-ceri <cutelilpossum@gmail.com>
2023-04-06 21:45:36 -0400

    changed the server stuff

--- server.js
+++ server.js
@@ -11,14 +11,11 @@ app.use(express.static("public"));
 app.use(require("./routes"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social-network-API",
+  process.env.MONGODB_URI || "mongodb://localhost:27017",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
-
-// Use this to log mongo queries being executed!
-mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
